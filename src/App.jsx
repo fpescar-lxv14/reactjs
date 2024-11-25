61bd7ab600a5104cd3cedd9282523a985c50df3d
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-11-25 16:47:31 -0300

    refactorizar constantes a json

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
+// Recursos
+import reactLogo from "./assets/react.svg";
+import menu from "./assets/menu.json";
+import social from "./assets/social.json";
+// Componentes
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
-import reactLogo from "./assets/react.svg"
 
 function App() {
-  const social = [
-    { 
-      link: "https://facebook.com/profile", 
-      text: "facebook"
-    },
-    { 
-      link: "https://instagram/profile", 
-      text: "instagram"
-    },
-    { 
-      link: "https://api.whatsapp.com/send?phone=1103030456&text=Buenos+dias,+quisiera+hacer+una+consulta...", 
-      text: "whatsapp"
-    }
-  ]
-  const menu = [
-    {
-      link: "#", text: "inicio"
-    },
-    {
-      link: "#services", text: "servicios"
-    },
-    {
-      link: "#contact", text: "contacto"
-    }
-  ]
   return (
     <>
     <Nav title="RJS" links={menu}/>
