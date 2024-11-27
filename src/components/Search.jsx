3e1e077dccfb0c0e22d88383e946463ec308f3ee
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-11-27 18:50:35 -0300

    implementar contexto en busqueda

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,11 @@
+import { useContext } from "react"
+import { StateContext } from "../providers/State"
+
 export default function Search() {
+    const { setParam, searchResults } = useContext(StateContext)
 return (
     <>
-    
+        <input type="text" onInput={(e) => setParam("q", e.target.value)}/>
+        <button onClick={searchResults}>buscar</button>
     </>
 )}
