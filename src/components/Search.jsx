3248369addddc5ce0122140ba4b227fca5a072a4
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-11-29 15:00:10 -0300

    Definir componente de busqueda con formulario

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,26 @@
-import { useContext } from "react"
+import { useContext, useRef } from "react"
 import { StateContext } from "../providers/State"
 
 export default function Search() {
-    const { setParam, searchResults } = useContext(StateContext)
-return (
-    <>
-        <input type="text" onInput={(e) => setParam("q", e.target.value)}/>
-        <button onClick={searchResults}>buscar</button>
-    </>
+    const qRef = useRef(), limitRef = useRef();
+    const {setParam, searchResults } = useContext(StateContext);
+    const get = (ref) => ref.current.value
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setParam('q', get(qRef));
+        setParam("limit", get(limitRef));
+        searchResults()
+    }
+    return (
+    <form onSubmit={handleSubmit}>
+        <input ref={qRef}/>
+        <select ref={limitRef} defaultValue={20}>
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="50">50</option>
+            <option value="100">100</option>
+            <option value="150">150</option>
+        </select>
+        <button>buscar</button>
+    </form>
 )}
