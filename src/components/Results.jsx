5416650aca13ec0c6b5ae6cb61e71a1d3badbdb2
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-12-02 15:09:43 -0300

    automatizar columnas

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,13 +1,23 @@
-import { useContext } from "react";
+import { useContext, useLayoutEffect, useRef, useState } from "react";
 import { StateContext } from "../providers/State";
 import Post from "./Post";
 
 export default function Results() {
+    const gridRef = useRef()
+    const [cols, setCols] = useState(1)
     const {search:{q}, results} = useContext(StateContext);
     const found = results?.data && q.length >= 2;
+    const getColumns = (col = 320) => {
+        const width = gridRef.current.offsetWidth
+        setCols(Math.floor(width / col))
+    }
+    useLayoutEffect(() => {
+        window.addEventListener('resize', () => getColumns());
+        return(window.removeEventListener('resize',() => "eliminado"))
+    },[])
 return (
-    <section className="container">            
-        <h2>{found ? "Resultados de "+q : "No se realizaron Busquedas"}</h2>
+    <section ref={gridRef} className="container">            
+        <h2 style={{gridColumn: `span ${cols}`}}>{found ? "Resultados de "+q : "No se realizaron Busquedas"}</h2>
         { results?.data.map(item => <Post key={item.id} {...item}/>) }
     </section>
 )}
