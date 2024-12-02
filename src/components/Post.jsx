ffaf301536587b21a1ebbbebb746239dfe4a459f
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-12-02 13:51:52 -0300

    estructura de componentes Publicacion y Resultados

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import Media from "./Media";
 
-export default function Post({title, username, images, ...props}){
+export default function Post({title, username, images}){
     const [isOpen, setIsOpen] = useState(false)
     const handleClick = () => setIsOpen(!isOpen);
     return (
-    <article onClick={handleClick}>
+    <article className={"post " + (isOpen ? "active" : "")} onClick={handleClick}>
         <header>
             <h3>{title}</h3>
             <p><strong>Publicado por</strong> {username}</p>
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,15 +1,13 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { StateContext } from "../providers/State";
 import Post from "./Post";
 
 export default function Results() {
-    const [posts, setPosts] = useState([]);
     const {search:{q}, results} = useContext(StateContext);
     const found = results?.data && q.length >= 2;
-    useEffect(() => {setPosts(results?.data ?? [])},[results?.data]);
 return (
     <section className="container">            
         <h2>{found ? "Resultados de "+q : "No se realizaron Busquedas"}</h2>
-        { posts.map(item => <Post key={item.id} {...item}/>) }
+        { results?.data.map(item => <Post key={item.id} {...item}/>) }
     </section>
 )}
