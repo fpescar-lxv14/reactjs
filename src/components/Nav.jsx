115d8da8d6af55c2486a4d7d7d74842f76def671
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-12-02 16:23:07 -0300

    instalar boostrap y aplicar estilos a navegacion

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,11 +2,12 @@ import { NavLink } from "react-router"
 
 export default function Nav() {
     return (
-    <nav>
-        <ul>
-            <li><NavLink to="home">Pagina Principal</NavLink></li>
-            <li><NavLink to="login">Iniciar Sesion</NavLink></li>
-            <li><NavLink to="signin">Registrarse</NavLink></li>
+    <nav className="navbar navbar-dark navbar-expand bg-dark p-2">
+        <a href="/home" className="navbar-brand">FPR</a>
+        <ul className="navbar-nav">
+            <li><NavLink className={'nav-link'} to="home">Pagina Principal</NavLink></li>
+            <li><NavLink className={'nav-link'} to="login">Iniciar Sesion</NavLink></li>
+            <li><NavLink className={'nav-link'} to="signin">Registrarse</NavLink></li>
         </ul>
     </nav>
 )}
