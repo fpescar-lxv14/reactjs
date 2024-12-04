093787c0045fb7e93a27fb860c702dececdbe600
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-12-04 14:16:47 -0300

    agregar ejemplo basico de react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, NavLink, Routes, Route } from "react-router"
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <nav>
+        {/* Links de Navegacion (api History) */}
+        <NavLink to="/home">Inicio</NavLink>
+        <NavLink to="/gallery">Galeria</NavLink>
+        <NavLink to="/contact">Contacto</NavLink>
+      </nav>
+      {/* Switch (renderizado condicional) */}
+      <Routes>
+        {/* Evaluaciones de path y devuelve componente o contenido */}
+        <Route path="home" element={<h2>Estas en Casa</h2>} />
+        <Route path="gallery" element={<h2>Bienvenido a la Galeria</h2>} />
+        <Route path="contact" element={<h2>Contactanos cuando Quieras</h2>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 export default App
