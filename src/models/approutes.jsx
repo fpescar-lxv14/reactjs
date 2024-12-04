146acc1fbd88569ba90c857317f153548d408603
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-12-04 15:22:28 -0300

    definir estructura de datos navegacion y rutas

---
+++ src/models/approutes.jsx
@@ -0,0 +1,11 @@
+import Home from "../components/Home"
+import Login from "../components/Login"
+import Signin from "../components/Signin"
+import NotFound from "../components/NotFound"
+
+export const appRoutes = [
+    {paths: ["/", "/home", "/inicio", "/main"], Element: Home},
+    {paths: ["/login", "/sign", "/session"], Element: Login},
+    {paths: ["/signin", "/register", "/account"], Element: Signin},
+    {paths: ["*"], Element: NotFound},
+]
---
+++ src/models/navigation.jsx
@@ -0,0 +1,6 @@
+export const navigation = [
+    { link: "home", text: "Pagina Principal"},
+    { link: "login", text: "Iniciar Sesion"},
+    { link: "register", text: "Registrarse" },
+    { link: "pagina_nueva", text: "Pagina Nueva" }
+]
