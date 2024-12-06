e099c8aae7d2082d9f3efd31c85ecc4989375bdd
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-12-06 13:49:46 -0300

    agregar componente lista de tareas

--- src/components/Todos/index.jsx
+++ src/components/Todos/index.jsx
@@ -1,6 +1,23 @@
+import { useSelector } from "react-redux"
+
 export default function Todos(){
+    const {todos, filtered} = useSelector(s => s.todos)
+    const data = filtered.length ? filtered : todos
     return(
     <>
-    
+        <form action="">
+            
+        </form>
+        <ul>
+            {data?.map(item => <ItemList key={item.id} {...item}/>)}
+        </ul>
     </>
 )}
+const ItemList = ({id,title, description,completed}) =>
+    <li id={"todo_"+id}>
+        <p style={{backgroundColor: completed ? "#ec3" : "underline"}}>
+            <strong>{title}: </strong> {description}
+            <button>Editar</button>
+            <button>Eliminar</button>
+        </p>
+    </li>
