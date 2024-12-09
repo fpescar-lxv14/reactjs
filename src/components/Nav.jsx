ea9f8ae51a4bc0d5ef8c07086137f249a7b93e77
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-12-09 14:51:29 -0300

    implementar mui en navegacion

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,17 @@
+import { BottomNavigation, BottomNavigationAction, List } from "@mui/material"
+import { AlarmOn, Checklist, Person } from "@mui/icons-material"
 import { NavLink } from "react-router"
 export default function Nav(){
     return(
-    <nav>
-        <ul>
-            <li><NavLink to="counter">Contador</NavLink></li>
-            <li><NavLink to="session">Datos de Usuario</NavLink></li>
-            <li><NavLink to="todos">Lista de Tareas</NavLink></li>
-        </ul>
-    </nav>
+    <BottomNavigation>
+        <NavLink to="counter">
+            <BottomNavigationAction label="contador" icon={<AlarmOn/>} showLabel/>
+        </NavLink>
+        <NavLink to="session">
+            <BottomNavigationAction label="datos de usuario" icon={<Person/>} showLabel/>
+        </NavLink>
+        <NavLink to="todos">
+            <BottomNavigationAction label="lista de tareas" icon={<Checklist/>} showLabel/>
+        </NavLink>
+    </BottomNavigation>
 )}
